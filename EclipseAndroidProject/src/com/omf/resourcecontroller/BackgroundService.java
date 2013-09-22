7b2021387a8babefd9b6562ffea6aa29bd1f72e0
Paolo Carta <pcarta@ee.ethz.ch>
2013-09-22 21:13:04 +0200

    adding support for bound service

--- EclipseAndroidProject/src/com/omf/resourcecontroller/BackgroundService.java
+++ EclipseAndroidProject/src/com/omf/resourcecontroller/BackgroundService.java
@@ -14,8 +14,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.os.Messenger;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.omf.resourcecontroller.OMF.OMFMessage;
 import com.omf.resourcecontroller.OMF.XMPPClass;
@@ -40,10 +42,42 @@ public class BackgroundService extends Service {
 	//TopicName
 	private String topicName = null;
 	
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}	
+	/** Command to the service to display a message */
+    static final int MSG_SAY_HELLO = 1;
+
+    
+    
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        return mMessenger.getBinder();
+    }
+	
 
 	@Override
 	public void onCreate() {
